f1a963d9d07153fa812b9d62b358685a5ba2d565
minheewon <lkmj12qw@gmail.com>
2025-06-19 12:53:55 +0900

    마이페이지 UI, 각 버튼에 해당하는 페이지 이동 구현

--- src/components/pages/MyPage.tsx
+++ src/components/pages/MyPage.tsx
@@ -15,27 +15,29 @@ export default function MyPage({ user }: MyPageProps) {
       </div>
 
       <div className={styles.userInfoWrapper}>
-        <img src='/src/assets/img/button/myPageBtn.svg' alt='프로필 이미지' />
+        <img src='/src/assets/img/profileImg.svg' alt='프로필 이미지' className={styles.profileImg} />
 
         <div className={styles.userInfo}>
           <div className={styles.userName}>
-            <span>{user?.displayName}</span>님
+            <span>{user?.displayName || '사용자'}</span>님, 반가워요!
           </div>
           <div className={styles.userEmail}>이메일: {user?.email}</div>
-          <Link to='/mypage/changeinfo' className={styles.userChangeInfo}>
-             <img src='/src/assets/img/button/settingBtn.svg' alt='회원정보수정 버튼' />
-          </Link>
         </div>
+
+        <Link to='/mypage/changeinfo' className={styles.userChangeInfo}>
+          <img src='/src/assets/img/button/settingBtn.svg' alt='회원정보수정 버튼' />
+        </Link>
       </div>
+
 
       <div className={styles.buttonArea}>
         <Link to='/wishlist'>위시리스트</Link>
         <Link to='/mypage/orders'>구매 내역</Link>
       </div>
 
-      <Link to='/mykit'>My Kit 보기</Link>
+      <Link to='/mykit' className={styles.mykitBtn}>My Kit 보기</Link>
       
-      <button>로그아웃</button>
+      <button className={styles.logoutBtn}>로그아웃</button>
 
 
     </div>
