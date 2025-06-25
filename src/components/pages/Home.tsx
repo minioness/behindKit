2c5a02705261dbbb07db0ac8e90ed2433b298caa
minheewon <lkmj12qw@gmail.com>
2025-06-25 12:43:45 +0900

    카테고리 List UI 애니메이션 작업 추가

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -6,11 +6,27 @@ import Slider from '../common/Slider';
 import FilteredProductGrid from '../products/FilteredProductGrid';
 
 import styles from './Home.module.css';
+import cx from 'clsx';
+
+// 상품 카테고리 추출
+const categories = Array.from(
+  new Set(products.map((product) => product.category))
+);
+
+const CATEGORY_LIST = ['전체', ...categories];
 
 
 export default function Home() {
 
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('전체');
+  const [showCategoryList, setShowCategoryList] = useState(false);
+
+
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category);
+    setShowCategoryList(false);    // 목록 숨기기
+  }
 
   return (
     <div className={styles.homeContainer}>
@@ -29,14 +45,42 @@ export default function Home() {
         </div>
 
 
-        <button className={styles.categoryBtn}>
-          카테고리 
-          <img src='/src/assets/img/button/arrowIcon.svg' alt='화살표 아이콘' className={styles.arrowIcon}/>
-        </button>
+        <div className={styles.categoryBtnWrapper}>
+          <button 
+            className={styles.categoryToggleBtn} 
+            onClick={() => setShowCategoryList((prev)=> !prev)}
+          >
+            {selectedCategory === '전체' ? '카테고리' : selectedCategory}
 
-        <button className={styles.sortBtn}>
+            <img 
+              src='/src/assets/img/button/arrowIcon.svg' 
+              alt='화살표 아이콘' 
+              className={styles.arrowIcon}
+            />
+          </button>
+
+          
+          <div className={cx(styles.categoryGroup, {[styles.show]: showCategoryList })}>
+            {CATEGORY_LIST.map((category) => (
+              <button
+                key={category}
+                className={cx(styles.categoryBtn, selectedCategory === category ? styles.active : '')}
+                onClick= {() => handleCategoryClick(category)}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+          
+        </div>
+
+        <button className={styles.sortToggleBtn}>
           정렬
-          <img src='/src/assets/img/button/arrowIcon.svg' alt='화살표 아이콘' className={styles.arrowIcon}/>
+          <img 
+            src='/src/assets/img/button/arrowIcon.svg' 
+            alt='화살표 아이콘' 
+            className={styles.arrowIcon}
+          />
         </button>
 
       </div>
