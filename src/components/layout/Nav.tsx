06d9d18d52ae4537692fa62ad571fa37e7daea21
minheewon <lkmj12qw@gmail.com>
2025-06-29 22:34:57 +0900

    nav 햄버거 버튼 영역 구현

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,19 +1,49 @@
-import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import type { User } from "firebase/auth";
+import { signOut, type User } from "firebase/auth";
+import { auth } from "../../firebase";
+
 
 import { useRecoilState } from "recoil";
 import { cartState } from "../../recoil/cartAtom";
 
 import styles from "./Nav.module.css";
+import cx from 'clsx';
 
 
 
 export default function Nav({user}: { user: User | null }) {
 
-    const [cart, setcart] = useRecoilState(cartState);
+    const [cart, ] = useRecoilState(cartState);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
+
+    const hamburgerRef = useRef<HTMLDivElement>(null);
 
     const cartCount = cart.length
+
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+        if (
+            hamburgerRef.current && !hamburgerRef.current.contains(e.target as Node)
+        ) {
+            setMenuOpen(false);
+        }
+        };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+
+    const handleLogout = async() => {
+        await signOut(auth);
+        alert('로그아웃 되었습니다.');
+        navigate('/login');
+        setMenuOpen(false);
+    }
 
 
     return (
@@ -36,12 +66,20 @@ export default function Nav({user}: { user: User | null }) {
                     <Link to={user? '/mypage' : '/login'} className={styles.myPageBtn}>
                         <img src="/src/assets/img/button/myPageBtn.svg" alt="마이페이지"/>
                     </Link>
-                    <div className={styles.hamburgerBtn}>
-                        <button>
+
+
+                    <div className={styles.hamburgerBtn}  ref={hamburgerRef}>
+                        <button onClick={() => setMenuOpen(prev =>!prev)}>
                             <img src="/src/assets/img/button/hamburgerBtn.svg" alt="메뉴버튼"/>
                         </button>
 
-                        <div className={styles.menuArea}>햄버거버튼 누르면 나올 영역</div>
+                        <div className={cx(styles.hamburgerArea, { [styles.active]: menuOpen })}>
+                            <Link to={'/mypage'} onClick={() => setMenuOpen(false)} className={styles.mypageMove}>마이페이지</Link>
+                            <Link to={'/mypage/orders'} onClick={() => setMenuOpen(false)} className={styles.orderMove}>주문 내역</Link>
+                            <Link to={'/wishlist'} onClick={() => setMenuOpen(false)} className={styles.wishlistMove}>위시리스트</Link>
+                            <Link to={'/cart'} onClick={() => setMenuOpen(false)} className={styles.cartMove}>장바구니</Link>
+                            <button onClick={handleLogout} className={styles.logoutBtn} >로그아웃</button>
+                        </div>
                     </div>
                 </div>
 
